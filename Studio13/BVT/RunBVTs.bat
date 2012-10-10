58fb61720c9635df68225916c61e239323737e1f
Jim Shobe <jshobe@articulate.com>
2012-10-09 23:25:19 -0700

    Add batch file to run BVT tests

---
+++ Studio13/BVT/RunBVTs.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\SmartBear\TestComplete 9\Bin\TestComplete.exe" "C:\git\desktoptest\Studio13\BVT\StudioBVT.pjs" /r /p:StudioBVT /SilentMode /e /ExportLog:"C:\Studio13AutoTests\Logs\Studio13BVT.mht"
