280ad319e29e5067b422ecb85f5ef7ebe9f50da3
Jim Shobe <jshobe@articulate.com>
2012-10-23 23:52:22 -0700

    add publish and preview support, fix bug in copyresultsfile function

---
+++ Studio13/QuizMaker/RunQuizmakerBVTs.bat
@@ -0,0 +1,3 @@
+
+"C:\Program Files (x86)\SmartBear\TestComplete 9\Bin\TestComplete.exe" "C:\git\desktoptest\Studio13\QuizMaker\QuizMaker.pjs" /r /SilentMode /p:QuizMaker /e /ExportLog:"C:\git\desktoptest\Studio13\QuizMaker\QuizMaker\results.mht"
+"C:\Program Files (x86)\SmartBear\TestComplete 9\Bin\TestComplete.exe" "C:\git\desktoptest\Studio13\QuizMaker\QuizMaker.pjs" /r /SilentMode /p:QuizMaker /t:"KeywordTests|CopyResultsFile" /e
