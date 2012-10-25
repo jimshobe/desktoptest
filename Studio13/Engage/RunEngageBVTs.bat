a0a18cd7f0a4a8524003855e8e9a6ad0dac496e9
Jim Shobe <jshobe@articulate.com>
2012-10-24 23:59:27 -0700

    add Engage project

---
+++ Studio13/Engage/RunEngageBVTs.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\SmartBear\TestComplete 9\Bin\TestComplete.exe" "C:\git\desktoptest\Studio13\Engage\Engage.pjs" /r /SilentMode /p:BVT /e /ExportLog:"C:\git\desktoptest\Studio13\Engage\BVT\results.mht"
+"C:\Program Files (x86)\SmartBear\TestComplete 9\Bin\TestComplete.exe" "C:\git\desktoptest\Studio13\Engage\Engage.pjs" /r /SilentMode /p:BVT /t:"KeywordTests|CopyResultsFile" /e
+
