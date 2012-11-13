2239ce71364fd6172e43f463d26f9ce6db9deb77
Jim Shobe <jshobe@articulate.com>
2012-11-13 01:31:42 -0500

    add execution batch files

---
+++ TestTools/Batchfiles/win7auto/RunEngageBVTs.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\SmartBear\TestExecute 9\Bin\TestExecute.exe" "C:\Users\regular-user\Documents\GitHub\desktop-Test\Studio13\Engage\Engage.pjs" /r /SilentMode /p:BVT /e 
---
+++ TestTools/Batchfiles/win7auto/RunQuizmakerBVTs.bat
@@ -0,0 +1,2 @@
+
+"C:\Program Files (x86)\SmartBear\TestExecute 9\Bin\TestExecute.exe" "C:\Users\regular-user\Documents\GitHub\desktop-Test\Studio13\QuizMaker\QuizMaker.pjs" /r /SilentMode /p:QuizMaker /e 
